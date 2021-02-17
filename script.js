f1b55c63992b975b0ec006e0ef5517394c3b026f
Taufiq Azad Siam <towfiqazadsiam@gmail.com>
2021-02-17 14:56:19 +0600

    Completed The Guess Game

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random()*20+1);
+let secretNumber = Math.trunc(Math.random()*20+1);
+
+
 // document.querySelector('.number').textContent = secretNumber;
 
 let score = 20;
+let highScore = 0;
+
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number( document.querySelector('.guess').value);
@@ -20,6 +24,11 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector(".highscore").textContent = score; 
+        }
 
 
 
@@ -61,13 +70,14 @@ document.querySelector('.check').addEventListener('click', function(){
 
     document.querySelector('.again').addEventListener('click', function(){
         score = 20;
-        const secretNumber = Math.trunc(Math.random()*20+1);
+        secretNumber = Math.trunc(Math.random()*20+1);
         const guess = Number( document.querySelector('.guess').value);
-        document.querySelector('.number').textContent = '?';
-        document.querySelector(".guess").value = ' ';
+        document.querySelector('.number').textContent = '?' ;
+        Number(document.querySelector(".guess").value = ' ');
         document.querySelector('body').style.backgroundColor = '#222';
         document.querySelector('.number').style.width = '15rem';
         document.querySelector('.message').textContent = 'Start guessing...';
+        document.querySelector('.score').textContent = score; 
 
     
 
